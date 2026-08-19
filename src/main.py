c41e9dfda72147b9bae217ed8c1193136d1ac908
Gustavo Marquez <forteres@hotmail.com>
2026-08-19 15:48:12 -0300

    refactor: open_image | add: methods grayscale_average, grayscale_weighted

--- src/main.py
+++ src/main.py
@@ -14,11 +14,16 @@ from datetime import datetime
 # global vars
 img_default_path = Path(__file__).parent.parent/'images'
 
+# helper methods
+def open_image(img_path):
+    img = cv.imread(img_path)
+    assert img is not None, "file could not be read, check with os.path.exists()"
+    return img
+
 # methods lab-m1.1
 # inspect
 def inspect(img_path):
-    img = cv.imread(img_path)
-    assert img is not None, "file could not be read, check with os.path.exists()"
+    img = open_image(img_path)
     width,height,channels = img.shape
     print(f'width: {width}\nheight: {height}\nchannels: {channels}\npixels: {img.size}\ntype: {img.dtype}')
     for i in range(channels):
@@ -26,23 +31,20 @@ def inspect(img_path):
 
 # copy
 def copy(img_path, output_path):
-    img = cv.imread(img_path)
-    assert img is not None, "file could not be read, check with os.path.exists()"
+    img = open_image(img_path)
     tempImg = np.copy(img)
     cv.imwrite(output_path, tempImg)
 
 # channel_b
 def channel_b(img_path, output_path):
-    img = cv.imread(img_path)
-    assert img is not None, "file could not be read, check with os.path.exists()"
+    img = open_image(img_path)
     tempImg = np.copy(img)
     tempImg[:,:,1:3] = 0
     cv.imwrite(output_path, tempImg)
 
 # channel_g
 def channel_g(img_path, output_path):
-    img = cv.imread(img_path)
-    assert img is not None, "file could not be read, check with os.path.exists()"
+    img = open_image(img_path)
     tempImg = np.copy(img)
     tempImg[:,:,0] = 0
     tempImg[:,:,2] = 0
@@ -50,13 +52,26 @@ def channel_g(img_path, output_path):
 
 # channel_r
 def channel_r(img_path, output_path):
-    img = cv.imread(img_path)
-    assert img is not None, "file could not be read, check with os.path.exists()"
+    img = open_image(img_path)
     tempImg = np.copy(img)
     tempImg[:,:,0:2] = 0
     cv.imwrite(output_path, tempImg)
+
 # grayscale_average
+def grayscale_average(img_path, output_path):
+    img = open_image(img_path)
+    tempImg = np.copy(img)
+    tempImg = np.mean(tempImg, axis=2).astype(np.uint8)
+    cv.imwrite(output_path, tempImg)
+
 # grayscale_weighted
+def grayscale_weighted(img_path, output_path):
+    img = open_image(img_path)
+    weights = np.array([0.299, 0.587, 0.114])
+
+    tempImg = np.copy(img)
+    tempImg = np.dot(tempImg[...,:3], weights).astype(np.uint8)
+    cv.imwrite(output_path, tempImg)
 # quantize
 
 
@@ -79,3 +94,7 @@ if __name__ == '__main__':
         channel_g(args.input, args.output)
     elif args.operation == 'channel_r':
         channel_r(args.input, args.output)
+    elif args.operation == 'grayscale_average':
+        grayscale_average(args.input, args.output)
+    elif args.operation == 'grayscale_weighted':
+        grayscale_weighted(args.input, args.output)
