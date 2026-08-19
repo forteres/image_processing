8d0ada805c0292b58a3a62db739c3001dbee8c9c
Gustavo Marquez <forteres@hotmail.com>
2026-08-19 16:52:47 -0300

    refactor: enforce choices for argument -p

--- src/main.py
+++ src/main.py
@@ -5,12 +5,6 @@ import cv2 as cv
 from pathlib import Path
 from datetime import datetime
 
-# img[:,:,1:3] = 0
-# wtv = img[0,0,2]
-# print("\n algo:", wtv)
-# cv.imshow('a',img)
-# cv.waitKey()
-
 # global vars
 img_default_path = Path(__file__).parent.parent/'images'
 
@@ -72,6 +66,7 @@ def grayscale_weighted(img_path, output_path):
     tempImg = np.copy(img)
     tempImg = np.dot(tempImg[...,:3], weights).astype(np.uint8)
     cv.imwrite(output_path, tempImg)
+
 # quantize
 
 
@@ -80,7 +75,14 @@ if __name__ == '__main__':
 
     parser.add_argument('-i', '--input', type=str,default=os.path.join(img_default_path, 'input', 'donkey.jpg'), help='input path+file')
     parser.add_argument('-o', '--output', type=str,default=os.path.join(img_default_path, 'output', datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + '.jpg'), help='output path+file')
-    parser.add_argument('-p', '--operation', type=str,required=True, help='operation to perform')
+    parser.add_argument('-p', '--operation', type=str,required=True, help='operation to perform',
+                        choices=['inspect', 
+                                 'copy', 
+                                 'channel_b', 
+                                 'channel_g', 
+                                 'channel_r', 
+                                 'grayscale_average', 
+                                 'grayscale_weighted'])
     #parser.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
     args = parser.parse_args()
 
