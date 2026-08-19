0d332ccc9c0b55d69932e47b759e30eca1128910
Gustavo Marquez <forteres@hotmail.com>
2026-08-19 15:17:13 -0300

    add: methods copy, channel_b, channel_g, channel_r

--- src/main.py
+++ src/main.py
@@ -23,10 +23,38 @@ def inspect(img_path):
     print(f'width: {width}\nheight: {height}\nchannels: {channels}\npixels: {img.size}\ntype: {img.dtype}')
     for i in range(channels):
         print(f'channel {i} min: {np.min(img[:,:,i])} max: {np.max(img[:,:,i])} mean: {np.mean(img[:,:,i]):.3f}')
+
 # copy
+def copy(img_path, output_path):
+    img = cv.imread(img_path)
+    assert img is not None, "file could not be read, check with os.path.exists()"
+    tempImg = np.copy(img)
+    cv.imwrite(output_path, tempImg)
+
 # channel_b
+def channel_b(img_path, output_path):
+    img = cv.imread(img_path)
+    assert img is not None, "file could not be read, check with os.path.exists()"
+    tempImg = np.copy(img)
+    tempImg[:,:,1:3] = 0
+    cv.imwrite(output_path, tempImg)
+
 # channel_g
+def channel_g(img_path, output_path):
+    img = cv.imread(img_path)
+    assert img is not None, "file could not be read, check with os.path.exists()"
+    tempImg = np.copy(img)
+    tempImg[:,:,0] = 0
+    tempImg[:,:,2] = 0
+    cv.imwrite(output_path, tempImg)
+
 # channel_r
+def channel_r(img_path, output_path):
+    img = cv.imread(img_path)
+    assert img is not None, "file could not be read, check with os.path.exists()"
+    tempImg = np.copy(img)
+    tempImg[:,:,0:2] = 0
+    cv.imwrite(output_path, tempImg)
 # grayscale_average
 # grayscale_weighted
 # quantize
@@ -43,3 +71,11 @@ if __name__ == '__main__':
 
     if args.operation == 'inspect':
         inspect(args.input)
+    elif args.operation == 'copy':
+        copy(args.input, args.output)
+    elif args.operation == 'channel_b':
+        channel_b(args.input, args.output)
+    elif args.operation == 'channel_g':
+        channel_g(args.input, args.output)
+    elif args.operation == 'channel_r':
+        channel_r(args.input, args.output)
