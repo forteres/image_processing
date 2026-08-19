cff5a0d56f67fb7b393cd6796d08b6affc412b32
Gustavo Marquez <forteres@hotmail.com>
2026-08-19 14:54:49 -0300

    add inspect method

--- src/main.py
+++ src/main.py
@@ -1,23 +1,28 @@
 import argparse
+import os
 import numpy as np
 import cv2 as cv
 from pathlib import Path
 from datetime import datetime
 
-img = cv.imread(Path(__file__).parent.parent/'images/input/donkey.jpg')
-assert img is not None, "file could not be read, check with os.path.exists()"
+# img[:,:,1:3] = 0
+# wtv = img[0,0,2]
+# print("\n algo:", wtv)
+# cv.imshow('a',img)
+# cv.waitKey()
 
-width,height,channels = img.shape
-print("width: ", width ,"\nheight: ", height ,"\nchannels: ", channels ,"\npixels: ", img.size, "\ntype: ", img.dtype)
+# global vars
+img_default_path = Path(__file__).parent.parent/'images'
 
-img[:,:,1:3] = 0
-wtv = img[0,0,2]
-print("\n algo:", wtv)
-cv.imshow('a',img)
-cv.waitKey()
-
-#methods lab-m1.1
+# methods lab-m1.1
 # inspect
+def inspect(img_path):
+    img = cv.imread(img_path)
+    assert img is not None, "file could not be read, check with os.path.exists()"
+    width,height,channels = img.shape
+    print(f'width: {width}\nheight: {height}\nchannels: {channels}\npixels: {img.size}\ntype: {img.dtype}')
+    for i in range(channels):
+        print(f'channel {i} min: {np.min(img[:,:,i])} max: {np.max(img[:,:,i])} mean: {np.mean(img[:,:,i]):.3f}')
 # copy
 # channel_b
 # channel_g
@@ -30,8 +35,11 @@ cv.waitKey()
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='arguments for pdi_lab')
 
-    parser.add_argument('-i', '--input', type=str,default=Path(__file__).parent.parent/'images/input/donkey.jpg', help='input path+file')
-    parser.add_argument('-o', '--output', type=str,default=Path(__file__).parent.parent/'images/output/%s.jpg' % datetime.now().strftime("%Y-%m-%d_%H-%M-%S"), help='output path+file')
+    parser.add_argument('-i', '--input', type=str,default=os.path.join(img_default_path, 'input', 'donkey.jpg'), help='input path+file')
+    parser.add_argument('-o', '--output', type=str,default=os.path.join(img_default_path, 'output', datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + '.jpg'), help='output path+file')
     parser.add_argument('-p', '--operation', type=str,required=True, help='operation to perform')
     #parser.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
     args = parser.parse_args()
+
+    if args.operation == 'inspect':
+        inspect(args.input)
